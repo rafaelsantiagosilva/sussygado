967bc49a2e24eb43ff41809149f1fa4cc61049e3
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-07-02 20:45:09 -0300

    Adicionando um programa teste.

---
+++ calc/calc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 0, b = 0;
+    char op = '\0';
+
+    scanf("%f %c %f", &a, &op, &b);
+
+    if (op == '/' && b == 0)
+    {
+        printf("Não é possível dividir por 0.\n");
+        return 1;
+    }
+
+    switch (op)
+    {
+    case '+':
+        printf("%.2f+%.2f=%.2f", a, b, (a + b));
+        break;
+
+    case '-':
+        printf("%.2f-%.2f=%.2f", a, b, (a - b));
+        break;
+
+    case '*':
+        printf("%.2f*%.2f=%.2f", a, b, (a * b));
+        break;
+
+    case '/':
+        printf("%.2f/%.2f=%.2f", a, b, (a / b));
+        break;
+
+    default:
+        printf("Operação inválida.\n");
+        break;
+    }
+
+    return 0;
+}
